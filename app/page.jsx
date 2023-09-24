a66f99243f6cce67be34dd2c3a5cd7d1b039075b
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-09-24 18:21:54 -0400

    env

--- app/page.jsx
+++ app/page.jsx
@@ -13,6 +13,9 @@ export default function Home() {
   const [header, setHeader] = useState(false)
 
   const [banner, setBanner] = useState(true)
+  console.log('process.env.BLOB_READ_WRITE_TOKEN', process.env.BLOB)
+console.log('process.env.BLOB_READ_WRITE_TOKEN', process.env.BLOB_READ_WRITE_TOKEN)
+console.log('process.env.BLOB_READ_WRITE_TOKEN', process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN)
 
   return (
     <main className="flex flex-col items-center justify-evenly min-h-screen bg-gray-100">

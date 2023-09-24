a2fec7b7958e160b9dce2d41927fe6dd38b3b138
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-09-24 10:58:18 -0400

    deployment push

--- app/editor/page.jsx
+++ app/editor/page.jsx
@@ -3,10 +3,11 @@ import Editor from "./editor"
 import Header from "../components/header"
 export default function Page() {
 
+    
     return (
         <div className="flex flex-col items-center justify-evenly min-h-screen bg-gray-100">
             <Header/>
             <Editor/>
-        </div>
+        </div> 
     )
 }

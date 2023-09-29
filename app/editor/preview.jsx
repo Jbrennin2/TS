83ee6125ff1cca6a34ae232d698e59b242b36d11
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-09-28 20:18:05 -0400

    preview page confirmation

--- app/editor/preview.jsx
+++ app/editor/preview.jsx
@@ -7,9 +7,13 @@ export default function Preview({imageState, savedUrl}) {
 
     const [renders, setRenders] = useState(null);
     const [savedURL, setSavedURL] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [confirm, setConfirm] = useState(false);
 
 
     const saveImage = async () => {
+        console.log('saving image')
+        setLoading(true);
         const base64 = imageState
               // Convert Base64 to Uint8Array (binary)
         const byteCharacters = atob(base64);
@@ -20,6 +24,8 @@ export default function Preview({imageState, savedUrl}) {
         const byteArray = new Uint8Array(byteNumbers);
       
         // POST the binary data to /api/upload
+        console.log('saving image')
+
         const response = await fetch(`/api/upload?filename=my-image.png`, {
           method: 'POST',
           body: byteArray.buffer,
@@ -27,22 +33,40 @@ export default function Preview({imageState, savedUrl}) {
             'Content-Type': 'application/octet-stream'
           }
         });
+        console.log('saving image')
+
       
         const blob = await response.json();
         
         // Set the saved URL
         console.log(blob);
-        setSavedURL(blob);
+        setSavedURL(blob.url);
+        setLoading(false);
+        setConfirm(true)
         return blob;
       };
+
+      const continueCheckout = () => {return}
+
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
             <div className={"flex items-center justify-center w-[50vw]"}>
-                <img src={`data:image/png;base64,${imageState}`} className="border border-blue-400"/>
+                {!confirm ? (
+                    <img src={`data:image/png;base64,${imageState}`} className="border border-blue-400"/>
+
+                ) : (
+                    <img src={savedURL} className="border border-blue-400"/>
+
+                )}
             </div>
             <div className ="flex justify-center w-[30%] border border-blue-400">
+            {!confirm ? (
                 <button onClick={saveImage} className="text-xl text-center rounded bg-blue-400 hover:bg-blue-600 p-5">Confirm Image</button>
+
+            ) : (
+                <button onClick={continueCheckout} className="text-xl text-center rounded bg-blue-400 hover:bg-blue-600 p-5">Checkout</button>
+            )}
             </div>
         </div> 
     )

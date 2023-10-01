3ef015c588a7b99a0e70b76de6618b3d75285df4
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-10-01 14:22:42 -0400

    logging

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
 import { buffer } from 'micro';
+import { NextResponse } from 'next/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
@@ -9,6 +10,7 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 
 export async function POST(req) {
+  console.log('WEBHOOK HIT')
   const sig = req.headers['stripe-signature'];
   const buf = await buffer(req);
 
@@ -16,9 +18,10 @@ export async function POST(req) {
 
   try {
     event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
+    console.log('hit try')
   } catch (err) {
     console.error(`Webhook signature verification failed: ${err.message}`);
-    return res.status(400).send(`Webhook signature verification failed: ${err.message}`);
+    return NextResponse.json(`Webhook signature verification failed: ${err.message}`);
   }
 
   switch (event.type) {
@@ -33,6 +36,6 @@ export async function POST(req) {
       console.warn(`Unhandled event type: ${event.type}`);
   }
 
-  res.status(200).end();
+  NextResponse.json('done')
 }
 

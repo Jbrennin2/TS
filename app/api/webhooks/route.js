55a621c92f4a6cf73eb8876a903e0eb51efd1294
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-10-01 15:14:49 -0400

    wh

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -8,6 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
+export const bodyParser = false;
 
 export async function POST(req) {
   console.log('WEBHOOK HIT')
--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    api: {
-      customResolvers: {
-        '/api/webhooks': {
-          bodyParser: false,
-        },
-      }
-    }
-}
-
+const nextConfig = {}
 
 module.exports = nextConfig

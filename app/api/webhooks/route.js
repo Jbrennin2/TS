10608402cb80a6784a2e224c650dd6d036d8deed
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-10-01 15:17:21 -0400

    wh

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,42 +1,48 @@
-import Stripe from 'stripe';
-import { buffer } from 'micro';
-import { NextResponse } from 'next/server';
+// src/app/api/webhooks/checkout/route.ts
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2020-08-27',
+import Cors from "micro-cors";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE);
+
+const cors = Cors({
+  allowMethods: ["POST", "HEAD"],
 });
 
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
-export const bodyParser = false;
+const secret = process.env.STRIPE_WEBHOOK_SECRET || "";
 
 export async function POST(req) {
-  console.log('WEBHOOK HIT')
-  const sig = req.headers['stripe-signature'];
-  const buf = await buffer(req);
-
-  let event;
-
   try {
-    event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
-    console.log('hit try')
-  } catch (err) {
-    console.error(`Webhook signature verification failed: ${err.message}`);
-    return NextResponse.json(`Webhook signature verification failed: ${err.message}`);
+    const body = await req.text();
+    
+    const signature = headers().get("stripe-signature");
+    
+    const event = stripe.webhooks.constructEvent(body, signature, secret);
+    
+    if (event.type === "checkout.session.completed") {
+      if (!event.data.object.customer_details.email) {
+        throw new Error(`missing user email, ${event.id}`);
+      }
+     
+     if (!event.data.object.metadata.itinerary_id) {
+        throw new Error(`missing itinerary_id on metadata, ${event.id}`);
+      }
+    
+      updateDatabase(event.data.object.metadata.itinerary_id);
+      sendEmail(event.data.object.customer_details.email);
+    }
+    
+    return NextResponse.json({ result: event, ok: true });
+  } catch (error) {
+    
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "something went wrong",
+        ok: false,
+      },
+      { status: 500 }
+    );
   }
-
-  switch (event.type) {
-    case 'checkout.session.completed':
-      const session = event.data.object;
-      console.log(`Payment successful for session ID: ${session.id}`);
-      break;
-
-    // Add other event types to handle as needed
-
-    default:
-      console.warn(`Unhandled event type: ${event.type}`);
-  }
-
-  NextResponse.json('done')
 }
-

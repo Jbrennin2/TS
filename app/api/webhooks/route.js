8c4824993b25b514c39199d2fa866b3b7048b0ce
Brennin Joiner <brenninjoiner@Alls-MacBook-Pro.local>
2023-10-03 21:13:04 -0400

    wh log

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -17,7 +17,8 @@ export async function POST(req) {
       req.headers.get("stripe-signature") ,
       process.env.STRIPE_WEBHOOK_SECRET
     );
-
+    console.log(event);
+    
     const id = event.data.object.metadata.order_id;
     const status = 'paid'
 
